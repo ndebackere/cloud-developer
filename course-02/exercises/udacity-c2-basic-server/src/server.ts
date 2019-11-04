4db61acad96d63cdcb3272d4a78ea4652753e6f2
ndebackere <26265435+ndebackere@users.noreply.github.com>
2019-11-03 20:20:17 -0500

    test commit - had email mismatch with username

--- course-02/exercises/udacity-c2-basic-server/src/server.ts
+++ course-02/exercises/udacity-c2-basic-server/src/server.ts
@@ -12,7 +12,7 @@ import { Car, cars as cars_list } from './cars';
   const port = 8082; 
   
   //use middleware so post bodies 
-  //are accessable as req.body.{{variable}}
+  //are accessible as req.body.{{variable}}
   app.use(bodyParser.json()); 
 
   // Root URI call
